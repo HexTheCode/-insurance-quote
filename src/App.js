a08aff6b69a9fdfe271f12a374115f8ccd0395e9
HexTheCode <alopezgarcia380@gmail.com>
2020-08-17 12:09:51 +0200

    Styled Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import Header from "./components/Header";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 600px;
+`;
+
+const FormContainer = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
 
 function App() {
-  return <Header title="Cotizador de Seguros" />;
+  return (
+    <Container>
+      <Header title="Cotizador de Seguros" />
+      <FormContainer></FormContainer>
+    </Container>
+  );
 }
 
 export default App;
